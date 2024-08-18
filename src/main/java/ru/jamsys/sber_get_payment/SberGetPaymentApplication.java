358c2c301565df569f6094c0f6086da9b19709b8
sfinks <urasfinks@yandex.ru>
2024-08-18 20:32:15 +0300

    Initial commit

---
+++ src/main/java/ru/jamsys/sber_get_payment/SberGetPaymentApplication.java
@@ -0,0 +1,13 @@
+package ru.jamsys.sber_get_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberGetPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SberGetPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/jamsys/sber_get_payment/SberGetPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jamsys.sber_get_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SberGetPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
